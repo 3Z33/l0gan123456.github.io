fedb4a3519021b84c853a86d117519dc4218535d
l0gan123456 <panierlogan0@gmail.com>
2023-09-08 14:22:55 +0200

    Commit all

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cours PHP</title>
+    <!-- <link rel="stylesheet" href="./style.css"> -->
+    
+</head>
+<body>
+    <?php          /* pour écrire du php il faut ouvrir ses balises spécifiques  on ouvre <?php  et on referme avec  ?> */
+    echo "<p>Bonjour<p>"; /* chaque fin de ligne nécessite ; en php  /// si on fait echo "Bonjour" sans les balises <p> le texte s'affichera sur la page mais ne sera dans aucun balise. */
+    echo "<p>" . "Bonjour" . "</p>"; // on peut également faire de cette façon
+    $cookie = 10; // en php on définit une variable avec $ suivi du nom que l'on veut donner à cette variable
+    $phrase = "Je suis une phrase"; // on met des " " pour les strings
+    $nombre = 1.4; // float
+    $choix = true; // Booléens
+    /*
+    Array :
+        Indexés
+        Associatif
+    
+        Null => NULL
+    
+
+    */
+
+    $titre = '<br>Je suis titre';  //les balises <br> doivent être misent entre "" car elles ne sont par reconnues par php ( ou alors fermer la balise php avant de faire <br>)
+    echo $phrase;
+    echo $titre;
+
+    // les conditions
+
+    $condition = false;
+
+    if ($condition) {
+        echo "<br>Je passe ici donc c'est vrai";
+    } else {
+        echo "<br>Je passe par la donc c'est faux";
+    }
+
+    $code = 4227;
+
+    if ($code == 4227) { // == prend en compte que la variable sois égale  et === prend en compte que la variable sois égale et du même type
+        echo "<br>Le code est correct";
+    } else {
+        echo "<br>Le code n'est pas correct";
+    }
+
+    $couleur = "gris";
+    echo "<p>J'ai une autruche de couleur ". $couleur ."</p>";
+
+    if ($couleur == "rouge") {  // si
+        echo "J'ai une autruche de couleur rouge";
+    } else if ($couleur == "bleu") { // sinon si
+        echo "J'ai une autruche de couleur bleu";
+    } else { // sinon
+        echo "<p>J'ai pas d'autruche</p>";
+    }
+
+    // Ecriture Ternaire
+
+    $a = 15;
+    $un = $a > 11 ? 1 : 0; // si $a plus grand que 11 alors $un est égal à 1 sinon il est égal à 0 
+                          //on peut lire cela comme "$a est il plus grand que 11 ?   si oui il est égal à 1 ,  si non il est égal à 0
+
+    // Les Switch 
+
+    $tailles = 187;
+
+    switch ($tailles) {
+        case 120:
+            echo "<p>Tu es atteint de nanisme.</p>";
+            break;
+        case 150:
+            echo "<p>Tu es très petit(e)</p>";
+            break;
+        case 170:
+            echo "<p>Tu as une taille noramle</p>";
+            break;
+        case 200:
+            echo "<p>Bonjour Monsieur !</p>";
+            break;
+        default:
+            echo "<p>Tu n'existes pas !</p>";
+            break;  //ici le break; n'est pas obligatoire
+    }
+
+    // Lest tableaux
+    $tableau = [];
+    $tableau = [
+        42, // index 0
+        78, // index 1
+        48, // index 2
+        1486656, // index 3
+        "Une Autruche" // index 4
+    ]; 
+    echo $tableau[4];
+
+
+    echo '<pre>';  // on utilise '<pre>' pour mieux mettre en forme l'affichage du tableau sur la page
+    var_dump($tableau);  // var_dump() permet d'afficher l'integralité d'un tableau
+    echo '</pre>';
+
+
+    //La valeur de $resultat doit être égale à 30 en utilisant que les nombres qui se trouvent dans le tableau exo
+    $exo = [4, 12, 78, 98, 65];
+    $resultat = $exo[2] - ($exo[0] * $exo[1]);
+    echo $resultat;
+
+
+
+    // Tableaux Associatifs
+
+    $tab_assoc = [
+        "voiture" => "Volskwagen", // type string
+        "animal" => "Perroquet", // trype string
+        "chiffre" => 10, // type int
+        "calvitie" => true, // type Boolean
+        "legume" => null // type Null
+    ];
+
+    // J'ai fait un tableau avec 5 valeurs qui ont un index que j'ai défini moi même.
+    // Voiture est un index et Volkswagen est sa valeur
+    // Animale est un index et Perroquet est sa valeur
+    // etc ...
+
+    $tab_assoc['bras'] = false; // on ajoute un nouvel index (bras) avec sa valeur (false)  dans notre tableau  $tab_assoc
+
+    echo "<pre>"; var_dump($tab_assoc); echo "</pre>";
+
+    // La boucle while
+
+    $o = 0;
+    while(true) {
+        $o++;
+        echo "<p>Je passe dans la boucle while</p>";
+        if ($o == 10) {
+            break;  // sert à casser la boucle pour l'arrêter
+        }
+    }
+
+    // La boucle do-while
+
+    do {
+        echo 'Je passe dans la boucle do-while';
+    } while (false);
+
+    // La boucle for
+
+    for ($i=0; $i < 10; $i++) {
+        echo "<br>Je suis passé ". $i+1 ." fois dans la boucle for";
+        }
+    
+    
+    // Créer un tableau Associatif en mettant un index bras de type booléen et un index jambe qui va être un integer
+
+    // Si il n'y a pas de jambe ni de bras Tu es e-tronc !
+    //Sinon si il n'y a pas de bras, Pas de bras pas de chocoloat
+    // Sinon 
+    // Tu es basique donc tu es nul.
+
+
+    $tab_exo = [
+        "bras" => true,
+        "jambe" => 1,
+    ];
+
+    if ( $tab_exo["bras"] ==! true && $tab_exo["jambe"] ==! 1){  // on peut faire $tab_exo["jambe"] ==! 1   ou  !$tab_exo["bras]
+        echo "<br>Tu es un e-tronc !";
+    } else if ( $tab_exo["bras"] ==! true && $tab_exo["jambe"] == 1){
+        echo "<br>Pas de bras, pas de chocolat";
+    } else {
+        echo "<br>tu es basique donc tu es nul.";
+    }
+    
+        
+
+    
+
+
+     ?>
+    
+</body>
+</html>
