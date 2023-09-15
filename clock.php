6bd82e013bb25351c03316634fbbf2f74a994a19
l0gan123456 <panierlogan0@gmail.com>
2023-09-15 16:12:22 +0200

    Maj

--- clock.php
+++ clock.php
@@ -24,38 +24,83 @@
     <input type="number" name="seconde" id="seconde" value=15 min=0 max=59>
 
     <input type="submit" value="Submit">
-    <input type="reset" value="Reset">
-</form>
+    <button type="button" id="reset" onclick="ResetTimer()">Reset</button>
+
+    <button type="button" id="pause" onclick="PauseTimer()">Pause</button>
+    <button type="button" id="play">Play</button>
+</form> <!-- si on met un button avec  un type="button" dans un formulaire il sera obsolète, on peut donc retirer le type="button" -->
+
+
 
 <?php
-    
 
-    echo '<script>
+     
+     /*  on entre cette partie en echo '' pour que les valeurs heure, minute, seconde puissent être entrées   */
+
+    echo '<script>  
     var heure = ' .(!empty($_POST["heure"]) ? $_POST["heure"] : '0') .'
     var minute = ' .(!empty($_POST["minute"]) ? $_POST["minute"] : '0') .'
-    var seconde = ' .(!empty($_POST["seconde"]) ? $_POST["seconde"] : '15') .'
+    var seconde = ' .(!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') .'
+    function affichage() {
     
-    setInterval(function() {
         document.getElementById("minuteur").innerHTML= 
         `${(heure < 10 ? "0" : "") + heure}:
         ${(minute < 10 ? "0" : "") + minute}:
         ${(seconde < 10 ? "0" : "") + seconde}`
-        if (seconde <= 0) return
-        seconde--
-        if (seconde == 0)
+    }
+        function timer() {
+        affichage()
+        if (seconde <= 0 && minute <= 0 && heure <= 0) return
+    
+        if (seconde == 0) {
         if (minute > 0) {
-            seconde = 59
+            seconde = 60
             minute--
         } else {
             if (heure > 0) {
                 heure--
                 minute = 59
-                seconde = 59
+                seconde = 60
             }
         }
-}, 1000)
+} 
+    seconde--
 
-</script>'
+
+
+}
+
+
+    
+
+var interval = setInterval(timer, 1000)
+function PauseTimer() {
+    clearInterval(interval)
+    interval = null
+    document.getElementById("minuteur").style.opacity = "0.5"
+}
+
+function ResetTimer() {
+
+    heure = 0
+    minute = 0
+    seconde = 0
+}
+
+document.getElementById("play").addEventListener("click", function() {
+    if (interval == null) {
+    clearInterval(interval)
+    interval = setInterval(timer, 1000)
+    document.getElementById("minuteur").style.opacity = "0.5"       
+    }
+    
+
+
+
+
+
+})
+</script>';
 ?>
 
 
