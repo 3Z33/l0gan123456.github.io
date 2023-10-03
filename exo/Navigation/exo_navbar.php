3a39f930ffb2e6e0dd95e92e310c6b872182c1fe
l0gan123456 <panierlogan0@gmail.com>
2023-10-03 16:28:39 +0200

    Maj

---
+++ exo/Navigation/exo_navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="exo_navbar.css">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</head>
+<body>
+
+<!-- pour les icones, utiliser : "menu" et "chevron down"  -->
+
+<div class="champ">
+
+<ul class="bar">
+    <li class="title">Soft UI PRO</li>
+    <li class="element">Pages<box-icon name='chevron-down'></box-icon></li>
+    <li class="element">Account<box-icon name='chevron-down'></box-icon></li>
+    <li class="element">Blocks<box-icon name='chevron-down'></box-icon></li>
+    <li class="element">Docs<box-icon name='chevron-down'></box-icon></li>
+    <li class="btn">BUY NOW</li>
+    
+
+    
+    
+    
+</ul>
+
+</div>
+    
+</body>
+</html>
