3601f3888286de6a9d99abac900244c1e673a68a
l0gan123456 <panierlogan0@gmail.com>
2023-10-05 20:10:58 +0200

    Commit

---
+++ exo/Navigation/exo_footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="exo_footer.css">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+
+<footer>
+
+    <ul>
+        <li>Products</li>
+        <li>Design & Developement</li>
+        <li>UX Pros</li>
+        <li>Business Intelligence</li>
+        <li>Team Productivity</li>
+    </ul>
+
+    <ul>
+        <li>Learn & Support</li>
+        <li>Help & API Docs</li>
+        <li>Blog</li>
+        <li>Technical Support Chat</li>
+    </ul>
+
+    <ul>
+        <li>My Account</li>
+        <li>Manage My Subscriptions</li>
+        <li>Support Requests</li>
+    </ul>
+
+    <ul>
+        <li>Corporate</li>
+        <li>About</li>
+        <li>Careers</li>
+        <li>Legal</li>
+        <li>News & Events</li>
+    </ul>
+
+    <div class="trait"></div>
+    <div class="line"></div>
+
+    
+
+<div class="logo">
+    <span><box-icon name='facebook-circle' type='logo' color='#ffffff' size="35px" ></box-icon></span>
+    <span><box-icon name='twitter' type='logo' color='#ffffff' size="35px" ></box-icon></span>
+    <span><box-icon name='linkedin-square' type='logo' color='#ffffff' size="35px" ></box-icon></span>
+    <span><box-icon name='youtube' type='logo' color='#ffffff' size="35px" ></box-icon></span>
+
+</div>
+
+    <p class="copy">Copyright 2021 INFRAGISTICS. All Rights Reserved</p>
+
+<div class="terms">
+
+    <pre>Terms of Use              Privacy Policy              Cookies</pre>
+    
+    
+
+</div>
+
+
+
+
+</footer>
+</body>
+</html>
