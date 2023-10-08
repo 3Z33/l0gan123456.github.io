42b34b1e38648cd997ce554479b7dfe125b76d25
l0gan123456 <panierlogan0@gmail.com>
2023-10-08 17:43:43 +0200

    Commit

---
+++ exo/bar-a-chat/exo_bar-a-chat-gerant.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="exo_bar-a-chat-gerant.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- 3 bars ayant chacun un stock de chats -->
+<!-- on peut ajouter ou supprimer des chats du stock d'un bar -->
+<!-- on peut modifier la description d'un chat -->
+<!-- on peut rajouter un bar à la liste des bars -->
+
+<!-- chaque chats doit avoir un numéro de puce de 15 chiffre. chaque bars doit avoir une adresse -->
+
+
+        <table border="1">
+    <tr>
+        <td>bar 1</td>
+    </tr>
+    <tr>
+        <td>Ligne 2</td>
+    </tr>
+    <tr>
+        <td>Ligne 3</td>
+    </tr>
+    <tr>
+        <td>Ligne 4</td>
+    </tr>
+    <tr>
+        <td>Ligne 5</td>
+    </tr>
+        </table>
+
+        <br><br> <!-- ----------------------------------------->
+
+        <table border="1">
+    <tr>
+        <td>bar2</td>
+    </tr>
+    <tr>
+        <td>Ligne 2</td>
+    </tr>
+    <tr>
+        <td>Ligne 3</td>
+    </tr>
+    <tr>
+        <td>Ligne 4</td>
+    </tr>
+    <tr>
+        <td>Ligne 5</td>
+    </tr>
+        </table>
+
+        <br><br> <!-- ----------------------------------------->
+
+        <table border="1">
+    <tr>
+        <td>bar3</td>
+    </tr>
+    <tr>
+        <td>Ligne 2</td>
+    </tr>
+    <tr>
+        <td>Ligne 3</td>
+    </tr>
+    <tr>
+        <td>Ligne 4</td>
+    </tr>
+    <tr>
+        <td>Ligne 5</td>
+    </tr>
+        </table>
+
+        <br><br> <!-- ----------------------------------------->
+
+       
+        
+    </table>
+</div>
+</body>
+</html>
---
+++ exo/bar-a-chat/exo_bar-a-chat.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="exo_bar-a-chat.css">
+    
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<div class="gerant">gérant du bar</div>
+
+
+
+    
+</body>
+</html>
